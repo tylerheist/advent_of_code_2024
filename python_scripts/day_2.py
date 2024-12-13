ead4a0bc56d9fc937475c1fcd255daae508e05ad
Tyler Heist <tylerheist@gmail.com>
2024-12-13 14:27:15 -0600

    day 2 puzzle

--- python_scripts/day_2.py
+++ python_scripts/day_2.py
@@ -8,7 +8,7 @@ def parse_input(fi: Path) -> list[str]:
 
 def is_report_safe(report: str) -> bool:
     is_increasing = None
-    numbers = int(report.split())
+    numbers = [int(num) for num in report.split()]
     for idx in range(len(numbers)-1):
         diff = (numbers[idx] - numbers[idx+1])
         if abs(diff) == 0 or abs(diff) > 3:
@@ -20,7 +20,20 @@ def is_report_safe(report: str) -> bool:
             return False
     return True
 
+def is_report_safe_dampened(report: str) -> bool:
+    if is_report_safe(report):
+        return True
+    numbers = report.split()
+    for idx in range(len(numbers)):
+        numbers_copy = numbers[:]
+        numbers_copy.pop(idx)
+        if is_report_safe(' '.join(numbers_copy)):
+            return True
+    return False
+
 if __name__ == "__main__":
     reports = parse_input(INPUT_LOCATION_PATH)
     reports_safety = [is_report_safe(report) for report in reports]
+    reports_dampened_safety = [is_report_safe_dampened(report) for report in reports]
     print(f"Total Number of Safe Reports: {sum(reports_safety)}")
+    print(f"Total Number of Dampened Safe Reports: {sum(reports_dampened_safety)}")
