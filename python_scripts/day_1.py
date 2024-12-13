ca5929a42ca49d134c568560090d4252487be0c2
Tyler Heist <tylerheist@gmail.com>
2024-12-13 13:54:40 -0600

    Day 1 Part 2

--- python_scripts/day_1.py
+++ python_scripts/day_1.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+from collections import Counter
 
-INPUT_LOCATION_PATH = Path('../inputs/day_1.txt')
+INPUT_LOCATION_PATH = Path('inputs/day_1.txt')
 
 def parse_input(fi: Path) -> list[list[int]]:
     left_list, right_list = [], []
@@ -16,6 +17,14 @@ def find_total_distance(left_list, right_list) -> int:
     distances = [abs(left_list[x] - right_list[x]) for x in range(len(left_list))]
     return sum(distances)
 
+def calculate_similarity_score(left_list, right_list) -> int:
+    count_right = Counter(right_list)
+    similarity_score = 0
+    for item in left_list:
+        similarity_score += item * count_right[item]
+    return similarity_score
+
 if __name__ == "__main__":
     lists = parse_input(INPUT_LOCATION_PATH)
-    print(find_total_distance(lists[0], lists[1]))
+    print(f"Total Distance: {find_total_distance(lists[0], lists[1])}")
+    print(f"Similarity Score: {calculate_similarity_score(lists[0], lists[1])}")
